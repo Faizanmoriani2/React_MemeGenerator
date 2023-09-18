c658c345c7f160ff86de59b34ff01eb9c47b4dcc
Faizan Moriani <46195130+Faizanmoriani2@users.noreply.github.com>
2023-09-18 12:05:11 +0500

    First Commit
    Added the project main files.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "./components/Header"
+import Memes from "./components/Memes"
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Memes />
+        </div>
+    )
+}
